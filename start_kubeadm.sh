8813aa37a263727fa2810cf4ed18129f934d653e
Waqas Riaz <waqasriaz234@gmail.com>
2022-04-24 18:35:15 +0500

    Update start_kubeadm.sh

--- start_kubeadm.sh
+++ start_kubeadm.sh
@@ -1,4 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
+
+MASTER_NODE='master'
 
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
@@ -6,12 +8,14 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl taint node master  node-role.kubernetes.io/master:NoSchedule-
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
+
+kubectl taint node "$MASTER_NODE"  node-role.kubernetes.io/master:NoSchedule-
 
 
-
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
-
+#install kube-flannel for networking
 KUBE_FLANNEL_FILE=/tmp/kube-flannel.yml
 if test -f "$KUBE_FLANNEL_FILE"; then
     echo "$KUBE_FLANNEL_FILE exists."
@@ -22,7 +26,7 @@ else
     kubectl apply -f $KUBE_FLANNEL_FILE
 fi
 
-
+#setup ingress
 KUBE_INGRESS_FILE=/tmp/deploy.yml
 if test -f "$KUBE_INGRESS_FILE"; then
     echo "$KUBE_INGRESS_FILE exists."
@@ -33,5 +37,3 @@ else
     kubectl apply -f $KUBE_FLANNEL_FILE
 fi
 
-#kubectl apply -f $HOME/kube-flannel.yml
-#kubectl apply -f $HOME/ingress/deploy.yaml
