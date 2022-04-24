fd8c7b638b1f4c179e09e41cc28a7335283dd24f
Waqas Riaz <wriaz@NIS-PK657.local>
2022-04-24 19:08:00 +0500

    added run.sh for kubeadm installation

---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+set -ex 
+
+VERIFIED_OS="Ubuntu 20.04"
+
+install_kubeadm(){
+    echo "\n\n"
+    echo -e "${GREEN}Starting Installation${RESET}"
+    echo "\n\n"
+    sleep 3
+
+    echo "Installing Docker!!"
+    echo "\n\n"
+    sleep 3
+    ./install-docker.sh
+
+    echo "Installing Kubeadm!!"
+    echo "\n\n"
+    sleep 3
+    ./install-kubeadm.sh
+
+    echo "Starting Kubeadm"
+    echo "\n\n"
+    sleep 3
+    ./start-kubeadm.sh
+
+}
+
+ 
+if [[ $(lsb_release -rs) == "20.04" ]]; then 
+    echo -e "${GREEN}Compatible version of OS for this script${RESET}"
+    echo "\n\n"
+    install_kubeadm
+    
+else
+    echo -e "${RED}This script is only verified with $"VERIFIED_OS". Do you still want to continue (y/n)?${RESET}"
+read answer
+    if [ "$answer" != "${answer#[Yy]}" ] ;then
+        install_kubeadm
+    else
+        echo "Exiting without Installation" 
+        exit 0
+    fi  
+fi
